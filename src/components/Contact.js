a7e6912344cd5500921e1b10424ac3541d4cf4ae
Tito Cahyo <tito.cahyo@gmail.com>
2022-06-23 13:40:32 +0700

    parbaiki contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,54 +5,59 @@ import React from "react";
 const Contact = ({ data }) => {
   const cbContact = (element) => {
     return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          backgroundColor: "#ff9f1c",
-          margin: "1  em",
-          padding: "1em",
-          border: "0",
-          borderRadius: "25px",
-        }}
-      >
-        <img
-          src={element.photo}
-          style={{
-            width: "240px",
-            height: "240px",
-            border: "0px",
-            borderRadius: "50%",
-            padding: "2em",
-          }}
-        ></img>
+      <>
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            padding: "2em",
+            flexDirection: "row",
+            backgroundColor: "#ff9f1c",
+            margin: "1  em",
+            padding: "1em",
+            border: "0",
+            borderRadius: "25px",
           }}
         >
-          <h1>{element.name}</h1>
-          <p>{element.phone}</p>
-          <p>{element.email}</p>
+          <img
+            alt="gambar kucing"
+            src={element.photo}
+            style={{
+              width: "240px",
+              height: "240px",
+              border: "0px",
+              borderRadius: "50%",
+              padding: "2em",
+            }}
+          ></img>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "2em",
+            }}
+          >
+            <h1>{element.name}</h1>
+            <p>{element.phone}</p>
+            <p>{element.email}</p>
+          </div>
         </div>
-      </div>
+      </>
     );
   };
   const arrContactBaru = data.map(cbContact);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "70vw",
-        alignItems: "center",
-      }}
-    >
-      {arrContactBaru}
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "70vw",
+          alignItems: "center",
+        }}
+      >
+        {arrContactBaru}
+      </div>
+    </>
   );
 };
 
